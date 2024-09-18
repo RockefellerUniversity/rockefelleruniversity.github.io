a9d375aaa63eddd67e06b6937064e654f5c334ae
ThomasCarroll <tc.infomatics@gmail.com>
2024-09-17 22:12:00 -0400

    Move to docs

--- docs/makeSite.R
+++ docs/makeSite.R
@@ -21,7 +21,7 @@ yml_empty() %>%
   ) %>%
   yml_output(html_document(toc = TRUE,theme="cosmo", toc_float=TRUE,highlight = "textmate")) %>% 
   as.yaml %>% 
-  writeLines(con = file.path("site","_site.yml"))
+  writeLines(con = file.path("docs","_site.yml"))
 
 
-rmarkdown::render_site(input = file.path("site"))
+rmarkdown::render_site(input = file.path("docs"))
