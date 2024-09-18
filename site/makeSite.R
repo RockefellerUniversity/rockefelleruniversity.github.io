74b327553338a51475733ff65076dcf214629f20
ThomasCarroll <tc.infomatics@gmail.com>
2024-09-17 20:04:53 -0400

    Update site page

---
+++ site/makeSite.R
@@ -0,0 +1,27 @@
+require(tidyverse)
+require(ymlthis)
+require(yaml)
+yml_empty() %>%
+  yml_site_opts(
+    name = ,
+    output_dir =  ".",
+  ) %>%
+  yml_navbar(
+    title = "Bioinformatics Resource Center",
+    left = list(
+      navbar_page("Home", href = "index.html"),
+      navbar_page("Software", href = "software.html"),
+      navbar_page("Training", href = "training.html"),
+      navbar_page("Analysis", href = "analysis.html")
+    ),
+    right = list(
+                 navbar_page(icon="ion-social-github",href="https://github.com/RockefellerUniversity"),
+                 navbar_page(icon="ion-social-youtube",href="https://www.youtube.com/@brcru"),
+                 navbar_page(icon="ion-social-twitter",href="https://twitter.com/Rockefeller_BRC"))
+  ) %>%
+  yml_output(html_document(toc = TRUE,theme="cosmo", toc_float=TRUE,highlight = "textmate")) %>% 
+  as.yaml %>% 
+  writeLines(con = file.path(getwd(),"_site.yml"))
+
+
+rmarkdown::render_site()
